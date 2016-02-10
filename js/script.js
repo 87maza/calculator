82040ac8ab56cf25badb49abd6014640f04fedd5
87maza <87maza@gmail.com>
2016-02-09 22:39:01 -0800

    numbers appear on screen, working on add function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-var num = JSON.parse()
+var display = document.getElementById("screen");
+var result;
+function printToScreen(x){
+	display.value += x;
+	//+= because you concatenate the strings when x is greater than single digits
+}
+
+function add(){
+	var num1 = parseInt(display.value);
+	var oper = "+";
+	var num2 = 
+}
+
+function calculate(){
+
+}
+
+function clear(x){
+	// display.value === 0;
+	// console.log(display.value);
+}
