4f34e8cd631feb3b6bda48091f32eb69ff58d845
87maza <87maza@gmail.com>
2016-02-14 23:55:36 -0800

    all operators work, decimal still has issues

--- js/script.js
+++ js/script.js
@@ -21,16 +21,52 @@ function printToScreen(x){
 	//+= because you concatenate the strings when x is greater than single digits
 }
 
-var operator = document.getElementById('oper');
-operator.addEventListener('click', function(){
+var add = document.getElementById('add');
+add.addEventListener('click', function(){
 	num1 = parseFloat(display.value);
-	sign = operator.value;
+	sign = add.value;
+	display.value='';
+});
+
+var subtract = document.getElementById('subtract');
+subtract.addEventListener('click', function(){
+	num1 = parseFloat(display.value);
+	sign = subtract.value;
+	display.value='';
+});
+
+var divide = document.getElementById('divide');
+divide.addEventListener('click', function(){
+	num1 = parseFloat(display.value);
+	sign = divide.value;
+	display.value='';
+});
+
+var multiply = document.getElementById('multiply');
+multiply.addEventListener('click', function(){
+	num1 = parseFloat(display.value);
+	sign = multiply.value;
 	display.value='';
 });
 
 var equals = document.getElementById('equals');
 equals.addEventListener('click',function(){
 	num2 = parseFloat(display.value);
+	if (num2 === 0 && sign === divide.value){
+		display.value = "don't divide by 0";
+		return;
+	}
+	else if (num2 === NaN){
+		var arrayNaN = num2.split('')
+		var split = arrayNaN.filter(checker);
+		num2 = split.join('');
+		return split.join('');
+
+			function checker(x){
+				return Boolean(x);
+			}
+
+	}
 	console.log(num1 + sign + num2);
 	result = parseFloat(oper[sign](num1,num2));
 	console.log(result);
@@ -63,18 +99,18 @@ clear.addEventListener('click', function(){
 
 
 
-var decimal = document.getElementById('decimal');
-decimal.addEventListener('click', function(){
-	console.log("ipresseddec");
-	var dotCheck = display.value;
-	if (dotCheck.indexOf(".") >= 0) {
-		console.log(dotCheck.indexOf("."));
-		return;
-	}
-	else {
-		dotCheck += ".";
-		// console.log("passed else");
-		// console.log(dotCheck.indexOf(".");
-	}
+// var decimal = document.getElementById('decimal');
+// decimal.addEventListener('click', function(){
+// 	console.log("ipresseddec");
+// 	var dotCheck = display.value;
+// 	if (dotCheck.indexOf(".") >= 0) {
+// 		console.log(dotCheck.indexOf("."));
+// 		return;
+// 	}
+// 	else {
+// 		dotCheck += ".";
+// 		// console.log("passed else");
+// 		// console.log(dotCheck.indexOf(".");
+// 	}
 
-});
+// });
