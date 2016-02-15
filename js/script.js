06fdc879fb55345a6e0b25809bd9953d6eab0f69
87maza <87maza@gmail.com>
2016-02-14 22:24:15 -0800

    add button works but other operators dont

--- js/script.js
+++ js/script.js
@@ -1,32 +1,72 @@
 var display = document.getElementById("screen");
 var result;
+var num1;
+var num2;
+	var oper = {
+		'+': function(a, b) {
+	        return a + b;
+	    },
+	    '*': function(a, b) {
+	        return a * b;
+	    },
+	    '/': function(a, b) {
+	        return a / b;
+	    },
+	    '-': function(a, b) {
+	        return a - b;
+	    }
+	}
 function printToScreen(x){
 	display.value += x;
 	//+= because you concatenate the strings when x is greater than single digits
 }
 
-function add(){
-	var num1 = parseFloat(display.value);
-	var oper = "+";
-	var num2 = parseFloat(display.value);
-}
+var operator = document.getElementById('oper');
+operator.addEventListener('click', function(){
+	num1 = parseFloat(display.value);
+	sign = operator.value;
+	display.value='';
+});
 
-function calculate(){
+var equals = document.getElementById('equals');
+equals.addEventListener('click',function(){
+	num2 = parseFloat(display.value);
+	console.log(num1 + sign + num2);
+	result = parseFloat(oper[sign](num1,num2));
+	console.log(result);
+	display.value = result;
+});
 
-}
+//old command to make things work
+// var add = document.getElementById('add');
+// add.addEventListener('click', function(){
+// 	num1 = parseFloat(display.value);
+// 	var oper = "+";
+// 	display.value = ""
+// 	return;
+// });
+// var equals = document.getElementById('equals');
+// equals.addEventListener('click',function(){
+// 	num2 = parseFloat(display.value);
+
+// }
+
+
 
 var clear = document.getElementById('clear');
 clear.addEventListener('click', function(){
 	//clear button temp fix, i tried using the html onclick, but it never worked, hand rolled vanilla js works
 	display.value = '';
+	window.location.reload();
 	console.log("ipressedc");
 });	
 
 
 
-function decimal(){
+var decimal = document.getElementById('decimal');
+decimal.addEventListener('click', function(){
 	console.log("ipresseddec");
-	var dotCheck = display.value
+	var dotCheck = display.value;
 	if (dotCheck.indexOf(".") >= 0) {
 		console.log(dotCheck.indexOf("."));
 		return;
@@ -37,4 +77,4 @@ function decimal(){
 		// console.log(dotCheck.indexOf(".");
 	}
 
-}
+});
